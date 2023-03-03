13b72c6445f6f1b068217e5c864eb998cfe4f791
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-03-03 11:10:07 +0100

    image gallery

--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,14 @@
 <div v-if="pending" class="w-screen h-screen bg-black flex items-center justify-center text-white text-9xl">
     Loading ...
 </div> -->
-
 <div class="w-full flex-col flex py-20 items-center bg-gradient-to-b from-topGradient">
-    <img :src="index.story.content.logo" alt="" class="w-20">
-    <img :src="index.story.content.title" alt="" class="w-64 pt-3 pb-1">
-    <p class="font-medium text-darkBlue/50 text-sm ">{{ index.story.content.description }}</p>
+    <img :src="index.content.logo" alt="" class="w-20">
+    <img :src="index.content.title" alt="" class="w-64 pt-3 pb-1">
+    <p class="font-medium text-darkBlue/50 text-sm ">{{ index.content.description }}</p>
 </div>
 
 
-<div class="max-w-6xl mx-auto pt-20 lg:px-7 grid grid-cols-3 lg:grid-cols-7 md:grid-cols-6 sm:grid-cols-5 pb-48">
+<div class="max-w-6xl mx-auto lg:px-7 grid grid-cols-3 lg:grid-cols-7 md:grid-cols-6 sm:grid-cols-5 pb-48">
     <nuxt-link :to="`/icons/${story.slug}`" v-for="story in icons.stories" class="flex flex-col items-center pb-4 h-36 rounded-xl hover:border border-darkBlue/10 ">
         <div class="h-full grid items-center">
             <img :src="story.content.icon" alt="icon" class="">
@@ -21,7 +20,6 @@
         <p class="text-sm">{{ story.content.title }}</p>
     </nuxt-link> 
 </div>
-
 
 <footer class="max-w-6xl mx-auto flex pb-8 pt-7 border-t border-darkBlue/10 text-xs text-darkBlue/50 justify-between">
     <div class="space-x-7">
@@ -43,14 +41,14 @@
   
 <script setup>
   const baseUrl = 'https://api.storyblok.com/v2/cdn/stories/'
-  const version = '&version=published'
   const token = '&token=uiHAQzxUn1tVtEmEZRrKZwtt'
+  const version = '&version=published'
 
-  const { data: icons } = await useFetch(`${baseUrl}?starts_with=icons${version+token}`)
-  const { data: index } = await useFetch(`${baseUrl}index?${version+token}`)
-
+  const { data: icons } = await useFetch(`${baseUrl}?starts_with=icons${version+token}&per_page=100`)
+  const index = await useAsyncStoryblok('index', { version: 'published' })
+//   const prova = await useStoryblok('icons/', { version: 'published' , starts_with: 'icons/' })
+//   console.log(prova);
 //   const story = await useAsyncStoryblok('home', { version: 'published' })
-
 
 //   const home = await useStoryblok("index", { version: "published" });
 //   const icons = await useStoryblok("", { "version": "published", "starts_with": "icons/" , "cv": 1676039583 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,16 +9,37 @@ module.exports = {
     "./app.vue",
   ],
   theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '15': '60px'
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'lp': '1400px',
+      '2xl': '1536px',
+    },
     fontFamily: {
       'sora': ['Sora' , 'sans-serif']
     },
     extend: {
       colors: {
         'white': '#ffffff',
+        'lightGray': '#f3f3f4',
+        'innerShadow': '#f0f1f2',
         'sidebar': '#f2f2f3',
         'sidebarActive': '#e9e9eb',
         darkBlue: '#090F24',
         topGradient: '#9487FF40',
+        lila: '#9487FF',
       },
     },
   },
